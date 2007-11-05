fc4b0a361131c67e177e7af047b4bb54afd1222b
Mark Probst <mark.probst@gmail.com>
2007-11-05 20:12:11 +0100

    Changed Depth Merge to be a unit filter.

--- examples/Combine/Depth Merge.mm
+++ examples/Combine/Depth Merge.mm
@@ -1,6 +1,7 @@
-filter depth_merge (image image1, image image2, image depth1, image depth2, 
-                    float overlap: 0.001-2 (1.0), float offset: -1-1 (0),
-                    float scale1: -1-1 (0.5), float scale2: -1-1 (-0.5))
+unit filter depth_merge (unit image image1, unit image image2,
+                         unit image depth1, unit image depth2, 
+                         float overlap: 0.001-2 (1.0), float offset: -1-1 (0),
+                         float scale1: -1-1 (0.5), float scale2: -1-1 (-0.5))
     d1 = gray(depth1(xy));
     d2 = gray(depth2(xy));
     frac = clamp(((d2 * scale2 - (d1 * scale1 + offset)) / overlap + 1) / 2, 0, 1);
