71f9f30166c3100d72968883d84ccb92dcb1f56d
Mark Probst <mark.probst@gmail.com>
2007-12-31 17:03:41 +0100

    Updated examples.

--- examples/Render/Darts Board.mm
+++ examples/Render/Darts Board.mm
@@ -1,6 +1,7 @@
 filter darts (image in, int subdivisions: 1-30 (10),
-              float distance: 0-1 (0.2), float width: 0-1 (0.05))
+              float distance: 0-1 (0.2), float width: 0-1 (0.2))
     ang=2*pi/subdivisions;
+    w=scale(width,0,1,0,distance);
     p=in(xy);
     q=if inintv((a-ang/4)%ang,0,ang/2)
         then
@@ -8,8 +9,7 @@ filter darts (image in, int subdivisions: 1-30 (10),
         else
             -p+1
         end;
-    dist=distance;
-    q=if inintv(r%dist,dist-width,dist)
+    q=if inintv(r%distance,distance-w,distance)
         then
             q
         else
--- examples/Render/Moire 1.mm
+++ examples/Render/Moire 1.mm
@@ -1,6 +1,6 @@
 filter moire1 ()
     q=t*2*pi;
-    abs(rgba:[sin(r*50+q)+sin(15*a+q),
-              sin(r*70+q)+sin(17*a+q),
-              sin(r*90+q)+sin(19*a+q),2])*0.5
+    abs(rgba:[sin(r*300+q)+sin(15*a+q),
+              sin(r*400+q)+sin(17*a+q),
+              sin(r*500+q)+sin(19*a+q),2])*0.5
 end
